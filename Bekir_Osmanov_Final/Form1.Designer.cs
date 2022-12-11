55f8c7e167bf7fd3218ddf69fe87cabf6bf4c1e8
BekirDoubleCup <bekirss@bk.ru>
2022-12-11 17:13:24 +0200

    Add project files.

---
+++ Bekir_Osmanov_Final/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Bekir_Osmanov_Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.snapBtn = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.scoreLbl = new System.Windows.Forms.Label();
+            this.highScoreLbl = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.startBtn.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startBtn.Location = new System.Drawing.Point(23, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(124, 56);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "START";
+            this.startBtn.UseVisualStyleBackColor = false;
+            this.startBtn.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapBtn
+            // 
+            this.snapBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.snapBtn.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.snapBtn.Location = new System.Drawing.Point(168, 12);
+            this.snapBtn.Name = "snapBtn";
+            this.snapBtn.Size = new System.Drawing.Size(124, 56);
+            this.snapBtn.TabIndex = 1;
+            this.snapBtn.Text = "SNAP";
+            this.snapBtn.UseVisualStyleBackColor = false;
+            this.snapBtn.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox.Location = new System.Drawing.Point(12, 71);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(584, 369);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // scoreLbl
+            // 
+            this.scoreLbl.AutoSize = true;
+            this.scoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLbl.Location = new System.Drawing.Point(337, 12);
+            this.scoreLbl.Name = "scoreLbl";
+            this.scoreLbl.Size = new System.Drawing.Size(76, 20);
+            this.scoreLbl.TabIndex = 3;
+            this.scoreLbl.Text = "SCORE:";
+            // 
+            // highScoreLbl
+            // 
+            this.highScoreLbl.AutoSize = true;
+            this.highScoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.highScoreLbl.Location = new System.Drawing.Point(337, 48);
+            this.highScoreLbl.Name = "highScoreLbl";
+            this.highScoreLbl.Size = new System.Drawing.Size(127, 20);
+            this.highScoreLbl.TabIndex = 4;
+            this.highScoreLbl.Text = "HIGH SCORE:";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 70;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Controls.Add(this.highScoreLbl);
+            this.Controls.Add(this.scoreLbl);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.snapBtn);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button snapBtn;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label scoreLbl;
+        private System.Windows.Forms.Label highScoreLbl;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
